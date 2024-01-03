2fafece15a0bfc5103ca7ec30af8f0a423bac4b2
GenVikGames <139751312+GenVikGames@users.noreply.github.com>
2024-01-03 19:28:04 +0700

    Задание_5.js

--- Задание_5.js
+++ Задание_5.js
@@ -4,7 +4,11 @@ const x = prompt('Введите число:');
 const n = prompt('Введите степень числа:');
 
 const sum = (x,n) => {
+    if(x ==0 || n ==0){
+       return 'Не натуральное число';
+    } 
+    else{
     res = x**n;
-    return res;
+    return res;}
 }
 console.log(sum(x,n))
